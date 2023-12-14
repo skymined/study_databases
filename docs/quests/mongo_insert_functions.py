a66d6f69a3c982e3e3b41ee8798bf04d014391a5
sky <adsky0309@gmail.com>
2023-12-14 10:16:45 +0900

    q

--- docs/quests/mongo_insert_functions.py
+++ docs/quests/mongo_insert_functions.py
@@ -1,11 +1,11 @@
 # fruit_info 리스트를 mongo 저장
 # 최소 2개의 function :connect, insert
 
-def connect() :  # mongodb에 있는 특정 collection에 연결하기 위한 함수
+def connect(address, data, colle) :  # mongodb에 있는 특정 collection에 연결하기 위한 함수
     from pymongo import MongoClient
-    mongoclient = MongoClient("mongodb://localhost:27017")
-    database = mongoclient['local']
-    collection = database['fruits']
+    mongoclient = MongoClient(address)
+    database = mongoclient[data]
+    collection = database[colle]
     return collection
 
 def put_fruits(collection, fruits) : #  collection에 내용 집어넣기 위한 함수
@@ -17,5 +17,5 @@ fruits = [{"과일명": "사과", "색상": "빨강", "원산지": "한국"},
         {"과일명": "오렌지", "색상": "주황", "원산지": "미국"},
         {"과일명": "수박", "색상": "초록", "원산지": "한국"}]
 
-collection = connect()
+collection = connect("mongodb://localhost:27017", "local", "fruits")
 put_fruits(collection, fruits)
