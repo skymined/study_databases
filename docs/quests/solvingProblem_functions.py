46afebb52f986e22b0afdaf19cbea01ab6b928e5
sky <adsky0309@gmail.com>
2023-12-15 10:21:46 +0900

    d

---
+++ docs/quests/solvingProblem_functions.py
@@ -0,0 +1,44 @@
+
+# list 불러오기
+
+class solvingproblem() : 
+    def __init__(self,collection) -> None:
+        self.question_list=[]
+        self.choices_list=[]
+        self.answer_list=[]
+        self.score_list=[]
+        self.user_score=[]
+        self.collection=collection
+        pass
+
+    def mongo (self):
+        from pymongo import MongoClient
+        mongoclient = MongoClient("mongodb://localhost:27017")
+        database = mongoclient["local"]
+        self.collection = database[self.collection]
+
+    def makeit(self):
+        result_list = self.collection.find({}, {"question" : 1,"choices":1, "answer":1, "score":1, "_id" :0})
+        for i in range(5):
+            self.question_list.append(result_list[i]["question"])
+            self.choices_list.append(result_list[i]["choices"])
+            self.answer_list.append(result_list[i]["answer"])
+            self.score_list.append(result_list[i]["score"])
+    
+    def updateit(self):
+        for j in range(1,6):
+            print("{}번 문제".format(j), self.question_list[j-1])
+            for i in range(1,5) : 
+                print("{}.".format(i), *self.choices_list[j-1][i-1])
+            print(self.score_list[j-1],"점")
+            user_answer = input("정답을 입력(번호가 아닌 답을 입력) : ")
+            self.collection.update_one({"numbering": str(j)}, { "$set":{"user_answer" : user_answer}})
+            if user_answer == self.answer_list[j-1] :
+                print("정답입니다!")
+                self.user_score.append(20)
+                self.collection.update_one({"numbering":str(j)}, {"$set":{"score":20}})
+            else : 
+                print("오답입니다.")
+                self.collection.update_one({"numbering":str(j)}, {"$set":{"score" :0}})
+            print("당신의 점수는",sum(self.user_score), "점입니다.")
+
--- docs/quests/solvingProblem_main.py
+++ docs/quests/solvingProblem_main.py
@@ -5,45 +5,11 @@
 # 어떤 문제를 사용자가 어떤 답을 했는지 알려주기
 
 # def connect(data, colle) :  # mongodb에 있는 특정 collection에 연결하기 위한 함수
-from pymongo import MongoClient
-mongoclient = MongoClient("mongodb://localhost:27017")
-database = mongoclient["local"]
-collection = database["solvingproblem"]
 
-# mongodb에 있는 문제 순차적으로 나오게 하기
-result_list = collection.find({}, {"question" : 1,"choices":1, "answer":1, "score":1, "_id" :0})
-pass
+from solvingProblem_functions import solvingproblem
 
-question_list=[]
-choices_list=[]
-answer_list=[]
-score_list=[]
+set = solvingproblem("solvingproblem")
 
-for i in range(5):
-    question_list.append(result_list[i]["question"])
-    choices_list.append(result_list[i]["choices"])
-    answer_list.append(result_list[i]["answer"])
-    score_list.append(result_list[i]["score"])
-    pass
-
-# 우선 재조립 성공! 각 리스트에 각각의 내용들이 담겼음.
-
-user_score=[]
-
-for j in range(1,5):
-    print("{}번 문제".format(j), question_list[j-1])
-    for i in range(1,5) : 
-        print("{}.".format(i), *choices_list[j-1][i-1])
-    print(score_list[j-1],"점")
-    user_answer = input("정답을 입력(번호가 아닌 답을 입력)")
-    collection.update_one({"numbering":j}, { "$set":{"user_answer" : user_answer}})
-    if user_answer == answer_list[j-1] :
-        print("정답입니다!")
-        user_score.append(20)
-        collection.update_many({"numbering":j}, {"$set":{"score":20}})
-    else : 
-        print("오답입니다.")
-        collection.update_many({"numbering":j}, {"$set":{"score" :0}})
-
-print("당신의 점수는",sum(user_score), "점입니다.")
-    
+set.mongo()
+set.makeit()
+set.updateit()
