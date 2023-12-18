cf2bd21e3a58212c299c86ee881889c4ba36160e
sky <adsky0309@gmail.com>
2023-12-18 10:44:47 +0900

    ㅇ

--- docs/quests/todolist_main.py
+++ docs/quests/todolist_main.py
@@ -2,5 +2,8 @@
 from todolist_functions import todolistproblem
 set = todolistproblem()
 
+# collection 불러오기
 set.mongo("todos_list", "participants", "participants_todos")
+
+#todolist 실행
 set.doit()
